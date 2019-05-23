f05b5fe1aad086d6d9827e534222ab1086c46680
zk555 <602281555@qq.com>
2019-05-23 17:44:35 +0800

    初始化

---
+++ src/main/java/com/gy/console/GyewayconsoleApplication.java
@@ -0,0 +1,13 @@
+package com.gy.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyewayconsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyewayconsoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gy/console/GyewayconsoleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gy.console;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GyewayconsoleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
